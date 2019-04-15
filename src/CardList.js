401a87f7d5e84a344b23102ae521acbb6e7dfc1e
DaveFres <nanyandave@gmail.com>
2019-04-15 14:31:02 +0300

    Added robots search

--- src/CardList.js
+++ src/CardList.js
@@ -5,18 +5,14 @@ const CardList = ({ robots }) => {
     const cardArray = robots.map((user, i) => {
         return (
             <Card
-                key = {i}
+                key={i}
                 id={robots[i].id}
                 name={robots[i].name}
                 email={robots[i].email}
             />
         );
     });
-    return (
-        <div>
-            {cardArray}
-        </div>
-    );
+    return <div>{cardArray}</div>;
 };
 
 export default CardList;
