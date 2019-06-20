540749112b8b70e8946a5d21c826b8869defbb4d
DaveFres <nanyandave@gmail.com>
2019-06-20 21:27:26 +0300

    Updates

---
+++ precache-manifest.9e4e4f4a6121c94b6242f0b86490b78b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8620ba80903ec7a76107",
+    "url": "/ReactRobo/static/css/main.32f83e5c.chunk.css"
+  },
+  {
+    "revision": "8620ba80903ec7a76107",
+    "url": "/ReactRobo/static/js/main.f03a7555.chunk.js"
+  },
+  {
+    "revision": "42701b20357af6f67f19",
+    "url": "/ReactRobo/static/js/runtime~main.898862fe.js"
+  },
+  {
+    "revision": "8278f371fd9dfa3390e8",
+    "url": "/ReactRobo/static/css/2.9d56057d.chunk.css"
+  },
+  {
+    "revision": "8278f371fd9dfa3390e8",
+    "url": "/ReactRobo/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ReactRobo/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "baec6d85bbdf50106469630fc0497ff1",
+    "url": "/ReactRobo/index.html"
+  }
+];
